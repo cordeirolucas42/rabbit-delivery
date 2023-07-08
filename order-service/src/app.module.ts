bcaf911da6536f803b5c2cb83173fc3e91e3471f
Lucas <lucas.cordeiro@rarolabs.com.br>
2023-07-08 14:59:20 -0300

    Implements mongoDB

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MenuModule } from './menu/menu.module';
 import { OrderModule } from './order/order.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb+srv://cordeirolucas42:4m0pVY5uBBSpkfw8@gearsets.4mscdf7.mongodb.net/', { dbName: "rabbit-delivery" }),
     MenuModule,
     OrderModule
   ]
--- order-service/src/order/order.service.ts
+++ order-service/src/order/order.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from "@nestjs/common";
 import { CreateOrderDTO, OrderDTO } from "./model/order.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Order } from "./model/order.schema";
 
 @Injectable()
 export class OrderService {
   constructor(
-    // public readonly database: Database
+    @InjectModel(Order.name) private orderModel: Model<Order>
   ) {}
 
   public async getOrderById(orderId: number): Promise<OrderDTO> {
-    return;
+    const order = this.orderModel.findOne({ id: orderId }).exec();
+    return order;
   }
 
-  public async placeOrder(orderToCreate: CreateOrderDTO): Promise<void> {
-    return;
+  public async placeOrder(createOrderDTO: CreateOrderDTO): Promise<void> {
+    const orderToCreate = this.ordersAdaptor(createOrderDTO);
+    const createdOrder = new this.orderModel(orderToCreate);
+    await createdOrder.save();
+  }
+
+  private ordersAdaptor(createOrderDTO: CreateOrderDTO) : Order {
+    const totalAmount = createOrderDTO.products.reduce((currentTotal, product) => {
+      return currentTotal + product.quantity*product.value;
+    }, 0);
+
+    return {
+      ...createOrderDTO,
+      totalAmount
+    }
   }
 }
